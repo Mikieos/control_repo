e05e6c4a3f97c5389101d1f1d110b62b5bd16e2f
Mikie O'Sullivan <mikie@zemi.io>
2018-01-25 14:17:02 +0000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC09CiNvdS5ZsQr/eJxJa2MMFcTDRtNQi3s4bXMppGTRUxq1cFOAZta/PH9Uf0IO9UBBxykEIvz0HUqool0ELqkx2EIti3Z7Jkm6McO9H0aCLLAycToUTJ5RVGkDa9kURbWhKgyUTRkxLoVhFnUK+BDt5deIhYKviuHpedEPOnG/PuToXZqKU579zjpbDv8KTJL+KBaaIhMq1N4GXiyiZdFAcpS9L12QhyZiCGcHWWTaZ300aHYFyj10IQOen7qyYht85D7kMB/JFnryRku9uPOJrBt4AWhSQj//r8En5nls30qiOgCmZbPQll2rM3EqddcjwsYfTPnQt2VPcCXkgsR root@master.puppet.vm',
 	}  
 }
