2655a8760df1920a61065e727429b6f60dfae118
Mikie O'Sullivan <mikie@zemi.io>
2018-01-29 10:40:08 +0000

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -3,13 +3,5 @@ class profile::web {
   
   class { 'nodejs':
     repo_url_suffix       => '6.x',
-    manage_package_repo       => false,
-    nodejs_dev_package_ensure => 'present',
-    npm_package_ensure        => 'present',
-  }
-
-  package { 'forever':
-    ensure   => 'present',
-    provider => 'npm',
   }
 }
