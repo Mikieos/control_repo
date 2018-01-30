6a704ee54913049ee8bab1406f3a4b55dc1acb52
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 09:14:31 +0000

    Create txservice.pp

---
+++ site/txservice/txservice.pp
@@ -0,0 +1,26 @@
+define txservice::installation (
+  $env = 'DS01',
+  $url = 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21'
+){
+  file { "/opt/${env}":
+    ensure => directory,
+  }
+  file { "/opt/${env}/configurations":
+    ensure => directory,
+  }
+  file { "/opt/${env}/etc":
+    ensure => directory,
+  }
+  file { "/opt/${env}/fscripts":
+    ensure => directory,
+  }
+  file { "/opt/${env}/lib":
+    ensure => directory,
+  }
+  file { "/opt/${env}/fscripts/txservice.sh":
+    ensure  => file,
+    content => epp('profile/foreverscript', {
+      env => $env
+    })
+  }
+}
