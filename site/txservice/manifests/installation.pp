49b49805909d7d0c956e70a2f3e6abe4369ad087
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 10:29:33 +0000

    Update installation.pp

--- site/txservice/manifests/installation.pp
+++ site/txservice/manifests/installation.pp
@@ -30,7 +30,7 @@ define txservice::installation (
   }
   file { "/var/log/forever/${title}/forever.log": 
     ensure  => file,
-    mode    => 755,
+    mode    => '755',
   }
   exec {"install txservice ${title}":
     command => "/opt/${title}/fscripts/txservice.sh update ${version}",
