fd79a836e9a1a564c50ceeaa37146bbb6396d419
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 09:18:08 +0000

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,22 +1,3 @@
 class profile::web {
-  include nginx
-  
-  class { 'nodejs':
-    repo_url_suffix       => '6.x',
-  }
-  
-  package { 'forever':
-    ensure   => 'present',
-    provider => 'npm',
-  }
-  
-  class {'profile::txservice':
-    env => 'FB01',
-    url => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21'
-  }
-  
-  class {'profile::txservice':
-    env => 'FB02',
-    url => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21'
-  }
+  include txservice
 }
