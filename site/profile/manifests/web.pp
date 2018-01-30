760f96e355a8fcc6e2dc11d3b3d7297afd599cc4
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 09:50:20 +0000

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -2,21 +2,21 @@ class profile::web {
   include txservice
   
   txservice::installation {'DS01':
-    server    => '192.168.100.72',
+    server    => '172.18.0.2',
     port      => 3001,
     nodeport  => 4001,
     version   => '1.25.0-21',
   }
   
   txservice::installation {'DS02':
-    server    => '192.168.100.72',
+    server    => '172.18.0.2',
     port      => 3002,
     nodeport  => 4002,
     version   => '1.25.0-21',
   }
   
   txservice::installation {'DS03':
-    server    => '192.168.100.72',
+    server    => '172.18.0.2',
     port      => 3003,
     nodeport  => 4003,
     version   => '1.25.0-21',
