5731585c7336016ef0f7fe7e5471a99fbe996afa
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 09:48:30 +0000

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -2,14 +2,23 @@ class profile::web {
   include txservice
   
   txservice::installation {'DS01':
-    url     => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21',
+    server    => '192.168.100.72',
+    port      => 3001,
+    nodeport  => 4001
+    version   => '1.25.0-21',
   }
   
   txservice::installation {'DS02':
-    url     => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21',
+    server    => '192.168.100.72',
+    port      => 3002,
+    nodeport  => 4002
+    version   => '1.25.0-21',
   }
   
   txservice::installation {'DS03':
-    url     => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21',
+    server    => '192.168.100.72',
+    port      => 3003,
+    nodeport  => 4003
+    version   => '1.25.0-21',
   }
 }
