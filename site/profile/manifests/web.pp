e1b1776150fa8f64fa28201d33480dbf45b5e5b8
Mikie O'Sullivan <mikie@zemi.io>
2018-01-30 09:20:19 +0000

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,3 +1,8 @@
 class profile::web {
   include txservice
+  
+  txservice::installation {'DS01':
+    env     => 'DS01',
+    url     => 'http://dev.mytransics.com/npm/package/tx-service/1.25.0-21',
+  }
 }
