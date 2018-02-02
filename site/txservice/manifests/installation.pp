9322fce6647b3964f8d8a232bdf01b49702934a1
Mikie O'Sullivan <mikie@zemi.io>
2018-02-02 10:26:04 +0000

    Update installation.pp

--- site/txservice/manifests/installation.pp
+++ site/txservice/manifests/installation.pp
@@ -35,7 +35,7 @@ define txservice::installation (
   }
   file { "/opt/${title}/configurations/config.json":
     ensure => file,
-    source => template('txservice/tx-service.config.json.erb', $config);
+    source => template('txservice/tx-service.config.json.erb', { config => $config});
   }
   exec {"install txservice ${title}":
     command => "/opt/${title}/fscripts/txservice.sh update ${version}",
