11f0074f776e4a0cbb5c09c6438edf8b17d6461c
Mikie O'Sullivan <mikie@zemi.io>
2018-02-02 12:52:15 +0000

    Update installation.pp

--- site/txservice/manifests/installation.pp
+++ site/txservice/manifests/installation.pp
@@ -3,12 +3,8 @@ define txservice::installation (
   $port     = 3001,
   $nodeport = 4001,
   $version  = '1.25.0-21',
-  $config   = lookup('config', {merge => hash, default_value => []})
+  $config   = lookup($title, {merge => hash, default_value => []})
 ){
-  info("installation started")
-  info("Config contents ${config}.")
-  info("my transics domain: ${config["mytransics"]["domain"]}");
-  info("my transics domain: ${config["redis"]["endpoints"]}");
   file { "/opt/${title}":
     ensure => directory,
   }
@@ -38,15 +34,14 @@ define txservice::installation (
     ensure  => file,
   }
   file { "/opt/${title}/configurations/config.json":
-    ensure => file,
-    # source => template('txservice/tx-service.config.json.erb', { config => $config});
+    ensure  => file,
+    replace => true
+    content => template('txservice/tx-service.config.json.erb', { config => $config});
   }
-  info("installing TX-Service")
   exec {"install txservice ${title}":
     command => "/opt/${title}/fscripts/txservice.sh install ${version}",
     require => [File["/opt/${title}/fscripts/txservice.sh"]],
   }
-  info("TX-Serivce Installed")
   nginx::resource::server { "${server}:${port}":
     listen_port => $port,
     proxy	=> "http://127.0.0.1:${nodeport}",
